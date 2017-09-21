13c373d7ab88e3cb98742f07f95f9cd12f7f0a14
michael <michaelgovoni@gmail.com>
2017-09-20 21:01:37 -0700

    Updated Contractors

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, redirect, url_for, render_template
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+from flask_mail import Mail
 
 # Create app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://salesadmin:my70pass@localhost/salesadmin'
-app.config['SECRET_KEY'] = 'super-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://irqgiazg:tNlIttpHWgyn54WGFUkNaZQPB3f42e7U@stampy.db.elephantsql.com:5432/irqgiazg'
+app.config['SECRET_KEY'] = 'XXYwqeh6323345fweWW'
 app.config['SECURITY_REGISTERABLE'] = True
-app.config['SECURITY_RECOVERABLE'] = True
-app.config['SECURITY_EMAIL_SUBJECT_REGISTER'] = True
-app.config['SECUTIRY_EMAIL_SUBJECT_PASSWORDLESS'] = True
-app.debug = True
+app.config['DEBUG'] = True
+# At top of file
+# After 'Create app'
+app.config['MAIL_SERVER'] = 'smtp.office365.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'michael.govoni@gexpro.com'
+#dont forget to change this configuration in production
+app.config['MAIL_PASSWORD'] = 'passwordholder'
+
+# Create database connection object
 db = SQLAlchemy(app)
 
 # Define models
@@ -19,10 +27,12 @@ roles_users = db.Table('roles_users',
         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
         db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
+
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,9 +43,27 @@ class User(db.Model, UserMixin):
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
 
+
+class Contractors(db.Model):
+    idc = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    contname = db.Column(db.String(255), unique=True)
+
+
+class Bidrecord(db.Model):
+    rid = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    contname = db.Column(db.String, db.ForeignKey('Contractors.contname'))
+    #contractor name key
+    #contname from above model?
+    crosell = db.Column(db.String(255), unique=True)
+    appval = db.Column(db.String(255), unique=True)
+    specproj = db.Column(db.String(255), boolean=True)
+    comments = db.Column(db.String(1000))
+
+
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
+
 
 # Views
 @app.route('/')
@@ -43,8 +71,7 @@ security = Security(app, user_datastore)
 def index():
     return render_template('InputPage.html')
 
-
-@app.route('/Main/<email>')
+@app.route('/profile/<email>')
 @login_required
 def profile(email):
     user = User.query.filter_by(email=email).first()
@@ -57,5 +84,7 @@ def post_user():
     db.session.commit()
     return redirect(url_for('index'))
 
+
+
 if __name__ == "__main__":
-    app.run('104.236.134.91')
+    app.run('localhost')
